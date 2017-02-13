cc69458894e2a13bcac14fee96ec1791dc37fd31
Younghyun Jo <younghyunjo99@gmail.com>
2017-02-14 07:30:07 +0900

    hw configuration file added

---
+++ hw_config.h
@@ -0,0 +1,13 @@
+#ifndef _HW_CONFIG_H_
+#define _HW_CONFIG_H_
+
+#define MOTOR_PIN		6
+#define MOTOR_TR_PIN 	7
+
+#define BUTTON_PIN		17
+
+#define BAT_APIN		6
+#define BAT_MIN_LEVEL	(3410/2)
+#define BAT_MAX_LEVEL	(4090/2)
+
+#endif //#ifndef _HW_CONFIG_H_
