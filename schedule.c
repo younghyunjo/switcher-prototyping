11392a7316330624b7b3166f9620e9d903e7cb5c
Younghyun Jo <younghyunjo99@gmail.com>
2017-02-26 22:49:04 +0900

    schedule_list, schedule_del, schedule_timer_evt_handler api added

--- schedule.c
+++ schedule.c
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include <stdbool.h>
+#include <time.h>
 
 #include "app_error.h"
 #include "fds.h"
@@ -10,7 +11,6 @@
 #include "nrf_log_ctrl.h"
 
 #define SCHEDULE_FILE_ID			0x01
-#define SCHEDULE_MAX_NR 			10
 
 #define INDEX_TO_RECORD_KEY(x)		((x)+1)
 
@@ -30,6 +30,36 @@ struct schedule_desc {
 	} schedules[SCHEDULE_MAX_NR];
 };
 static struct schedule_desc schedule_desc;
+
+static ret_code_t _schedule_write(uint8_t record_key, struct schedule *schedule)
+{
+	db_schema.day = schedule->day;
+	db_schema.hour = schedule->hour;
+	db_schema.minute = schedule->minute;
+	db_schema.on = schedule->on;
+
+	fds_record_chunk_t chunk = {
+		.p_data = (void*)&db_schema,
+		.length_words = 1,
+	};
+
+	fds_record_t record_to_write = {
+		.file_id = SCHEDULE_FILE_ID,
+		.key = record_key,
+		.data.p_chunks = &chunk,
+		.data.num_chunks = 1,
+	};
+
+	fds_record_desc_t desc;
+	memset(&desc, 0, sizeof(desc));
+
+	ret_code_t err_code = fds_record_write(&desc, &record_to_write);
+	if (err_code != FDS_SUCCESS) {
+		NRF_LOG_ERROR("fds_record_write failed. %d\r\n", err_code);
+	}
+
+	return err_code;
+}
 
 static ret_code_t _schedule_read(uint8_t record_key, struct schedule *schedule)
 {
@@ -55,21 +85,46 @@ static ret_code_t _schedule_read(uint8_t record_key, struct schedule *schedule)
 
 	fds_record_open(&desc, &record);
 
-	schedule->id = record_key;
 	struct schedule_db_schema *schema = (struct schedule_db_schema*)record.p_data;
 	schedule->day = schema->day;
 	schedule->hour = schema->hour;
 	schedule->minute = schema->minute;
 	schedule->on = schema->on;
 
-	NRF_LOG_INFO("schedule_read. record:%d day:%d hour:%d minute:%d on:%d\r\n",
-			schedule->id, schedule->day, schedule->hour, schedule->minute, schedule->on);
+	NRF_LOG_INFO("_schedule_read key:%d\r\n", record_key);
 
 	//TODO VALIDATION CHECK
 
 	fds_record_close(&desc);
 
 	return FDS_SUCCESS;
+}
+
+static ret_code_t _schedule_del(uint8_t record_key)
+{
+	fds_record_desc_t desc;
+	memset(&desc, 0, sizeof(fds_record_desc_t));
+
+	fds_find_token_t ft;
+	memset(&ft, 0, sizeof(fds_find_token_t));
+
+	ret_code_t err_code = fds_record_find(SCHEDULE_FILE_ID, record_key, &desc, &ft);
+	if (err_code != FDS_SUCCESS) {
+		if (err_code != FDS_ERR_NOT_FOUND) {
+			NRF_LOG_ERROR("fds_record_find error:%d\r\n", err_code);
+			return err_code;
+		}
+
+		NRF_LOG_INFO("%d empty\r\n", record_key);
+		return FDS_ERR_NOT_FOUND;
+	}
+
+	err_code = fds_record_delete(&desc);
+	if (err_code != FDS_SUCCESS) {
+		NRF_LOG_ERROR("fds_record_delete:%d\r\n", err_code);
+	}
+
+	return err_code;
 }
 
 static void _load_schedule()
@@ -83,9 +138,14 @@ static void _load_schedule()
 			schedule_desc.schedules[i].empty = true;
 			continue;
 		}
+		schedule.id = i;
 
 		schedule_desc.schedules[i].empty = false;
 		memcpy(&schedule_desc.schedules[i].schedule, &schedule, sizeof(struct schedule)); 
+
+		NRF_LOG_INFO("_load_schedule. id:%d day:%d hour:%d minute:%d on:%d\r\n",
+				schedule.id, schedule.day, schedule.hour, schedule.minute, schedule.on);
+
 	}
 
 	schedule_desc.loaded = true;
@@ -101,44 +161,66 @@ static void _fds_evt_handler(fds_evt_t const * const p_evt)
 		case FDS_EVT_WRITE:
 			NRF_LOG_INFO("written file_id:%d record_key:%d\r\n", p_evt->write.file_id, p_evt->write.record_key);
 			break;
+		case FDS_EVT_DEL_RECORD:
+			NRF_LOG_INFO("delete file_id:%d record_key:%d\r\n", p_evt->write.file_id, p_evt->write.record_key);
+			fds_gc();
+
+			break;
 		default:
 			break;
 	}
 }
 
-static ret_code_t _schedule_write(uint8_t record_key, struct schedule *schedule)
+void schedule_timer_evt_handler(time_t now)
 {
-	db_schema.day = schedule->day;
-	db_schema.hour = schedule->hour;
-	db_schema.minute = schedule->minute;
-	db_schema.on = schedule->on;
+	struct tm calendar_time;
+	gmtime_r(&now, &calendar_time);
 
-	fds_record_chunk_t chunk = {
-		.p_data = (void*)&db_schema,
-		.length_words = 1,
-	};
-
-	fds_record_t record_to_write = {
-		.file_id = SCHEDULE_FILE_ID,
-		.key = record_key,
-		.data.p_chunks = &chunk,
-		.data.num_chunks = 1,
-	};
-
-	fds_record_desc_t desc;
-	memset(&desc, 0, sizeof(desc));
-
-	ret_code_t err_code = fds_record_write(&desc, &record_to_write);
-	if (err_code != 0) {
-		NRF_LOG_ERROR("fds_record_write failed. %d\r\n", err_code);
+	if (now % 60) {
+		return;
 	}
 
-	return err_code;
+
+	NRF_LOG_INFO("day:%d\r\n", calendar_time.tm_wday);
+	NRF_LOG_INFO("hour:%d\r\n", calendar_time.tm_hour);
+	NRF_LOG_INFO("minute:%d\r\n", calendar_time.tm_min);
+
+	int i;
+	for (i=0; i<SCHEDULE_MAX_NR; i++) {
+		if (schedule_desc.schedules[i].empty)
+			continue;
+		if (schedule_desc.schedules[i].schedule.day == calendar_time.tm_wday &&
+				schedule_desc.schedules[i].schedule.hour == calendar_time.tm_hour &&
+				schedule_desc.schedules[i].schedule.minute == calendar_time.tm_min) {
+			NRF_LOG_INFO("Move Motor\r\n");
+		}
+	}
+}
+
+void schedule_list(struct schedule *schedule)
+{
+	int i;
+	for (i=0; i<SCHEDULE_MAX_NR; i++) {
+		if (schedule_desc.schedules[i].empty) {
+			schedule[i].id = SCHEDULE_ID_UNKNOWN;
+			continue;
+		}
+		memcpy(&schedule[i], &schedule_desc.schedules[i].schedule, sizeof(struct schedule));
+	}
+}
+
+void schedule_del(uint8_t id)
+{
+	if (id >= SCHEDULE_MAX_NR)
+		return;
+
+	_schedule_del(INDEX_TO_RECORD_KEY(id));
+	schedule_desc.schedules[id].empty = true;
 }
 
 uint8_t schedule_add(struct schedule *schedule)
 {
-	if (schedule->id == SCHDULE_ID_UNKNOWN) {
+	if (schedule->id == SCHEDULE_ID_UNKNOWN) {
 		int i;
 		for (i=0; i<SCHEDULE_MAX_NR; i++)
 			if (schedule_desc.schedules[i].empty)
@@ -146,13 +228,21 @@ uint8_t schedule_add(struct schedule *schedule)
 
 		if (i == SCHEDULE_MAX_NR) {
 			NRF_LOG_ERROR("schedule is full\r\n");
-			return SCHDULE_ID_UNKNOWN;
+			return SCHEDULE_ID_UNKNOWN;
 		}
 
 		schedule->id = i;
 	}
 
-	_schedule_write(INDEX_TO_RECORD_KEY(schedule->id), schedule);
+	ret_code_t err_code = _schedule_write(INDEX_TO_RECORD_KEY(schedule->id), schedule);
+	if (err_code != FDS_SUCCESS) {
+		NRF_LOG_ERROR("_schedule_write failed\r\n");
+		return SCHEDULE_ID_UNKNOWN;
+	}
+
+	schedule_desc.schedules[schedule->id].empty = false;
+	memcpy(&schedule_desc.schedules[schedule->id].schedule, schedule, sizeof(struct schedule));
+
 	return schedule->id;
 }
 
--- schedule.h
+++ schedule.h
@@ -1,7 +1,8 @@
 #ifndef _SCHEDULE_H_
 #define _SCHEDULE_H_
 
-#define SCHDULE_ID_UNKNOWN	0xff
+#define SCHEDULE_ID_UNKNOWN	0xff
+#define SCHEDULE_MAX_NR 	10
 
 struct schedule {
 	uint8_t id;
@@ -11,7 +12,11 @@ struct schedule {
 	uint8_t on;
 };
 
+void schedule_list(struct schedule *schedule);
+void schedule_del(uint8_t id);
 uint8_t schedule_add(struct schedule *schedule);
+
+void schedule_timer_evt_handler(time_t now);
 void schedule_init(void);
 
 #endif //#ifndef _SCHEDULE_H_
